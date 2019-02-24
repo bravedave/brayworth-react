2ffad967d7a234283fabbd084cdb30d9bcb4030d
Dave Bray <david@brayworth.com.au>
2019-02-24 20:11:31 +1300

    Updates

---
+++ precache-manifest.974498d783c530a40273a0ce07ad3757.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "53d1fa14fd2025f8d11c",
+    "url": "/brayworth-react/static/css/main.d028998e.chunk.css"
+  },
+  {
+    "revision": "53d1fa14fd2025f8d11c",
+    "url": "/brayworth-react/static/js/main.53d1fa14.chunk.js"
+  },
+  {
+    "revision": "cf8542c5b5a3b65f6838",
+    "url": "/brayworth-react/static/js/runtime~main.cf8542c5.js"
+  },
+  {
+    "revision": "88f0de4432b4f0fb9dfe",
+    "url": "/brayworth-react/static/css/2.c26e3b86.chunk.css"
+  },
+  {
+    "revision": "88f0de4432b4f0fb9dfe",
+    "url": "/brayworth-react/static/js/2.88f0de44.chunk.js"
+  },
+  {
+    "revision": "cdee0cb3b247c9c709d18285f1c9ab7f",
+    "url": "/brayworth-react/static/media/bg-1600.cdee0cb3.jpg"
+  },
+  {
+    "revision": "b04cbed14ae426d8b7710b27855484c1",
+    "url": "/brayworth-react/static/media/iphone-profile.b04cbed1.jpg"
+  },
+  {
+    "revision": "2c80d1d3a10e44c0fede8f0b2aec7443",
+    "url": "/brayworth-react/index.html"
+  }
+];
